58e17efd430e338f710f3e65b4a614efab09c39a
Sohib <sohiblodhi59@gmail.com>
2026-06-01 12:04:16 +0530

    My code

---
+++ myproject/javaprogram/program3.java
@@ -0,0 +1,12 @@
+public class program3 {
+    public static void main(String[] args) {
+
+    
+        System.out.println("My name is Sohib");
+        int a = 5;
+        int b =10;
+        int sum = a*b;
+         System.out.println("The of sum " + a +  " and "  + b +  " is "  +sum);
+
+    }
+}
