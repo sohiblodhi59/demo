e0728bdade205f0940bb6fbf3261faed410292a1
Sohib <sohiblodhi59@gmail.com>
2026-06-01 11:10:10 +0530

    second code

--- myproject/javaprogram/program1.java
+++ myproject/javaprogram/program1.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class program1 {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
---
+++ myproject/javaprogram/rogram2.java
@@ -0,0 +1,9 @@
+public class rogram2 {
+    public static void main (String []args) {
+        System.out.println("Welcome to java programming");
+        int a =10;
+        int b =20;
+        int sum =a+b;
+        System.out.println("The sum of " + a + " and " + b + " is " + sum);
+    } 
+}
